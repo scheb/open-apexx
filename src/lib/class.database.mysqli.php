08582603a9923721b8b3aa02493e7d69db370410
Christian Scheb <info@stylemotion.de>
2017-12-19 20:45:38 +0100

    Set SQL mode for MySQL >= 5.7

--- src/lib/class.database.mysqli.php
+++ src/lib/class.database.mysqli.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /***************************************************************\
 |                                                               |
@@ -19,21 +19,21 @@ if ( !defined('APXRUN') ) die('You are not allowed to execute this file directly
 
 
 class database extends mysqli {
-	
+
 	//Benchmark
 	var $benchmark = false;
 	var $benchdata = array();
 	var $benchtime=0;
-	
+
 	//Datenbankkonfiguration
 	var $conn;
 	var $querystring;
 	var $quieterror = false;
-	
-	
-	
+
+
+
 	////////////////////////////////////////////////////////////////////////////////// -> STARTUP + END
-	
+
 	//Verbindung herstellen
 	function database($server, $user, $password, $database, $utf8 = false) {
 		parent::__construct($server, $user, $password);
@@ -50,14 +50,17 @@ class database extends mysqli {
 		else {
 			$this->query("SET NAMES 'latin1'");
 		}
+		if ($this->server_version >= 50700) {
+			$this->query("SET sql_mode=''");
+		}
 	}
-	
-	
-	
-	//Verbindung schließen
+
+
+
+	//Verbindung schlieï¿½en
 	function close() {
 		parent::close();
-		
+
 		//Benchmark ausgeben
 		if ( $this->benchmark ) {
 			echo '<pre>';
@@ -66,27 +69,27 @@ class database extends mysqli {
 			echo '</pre>';
 		}
 	}
-	
-	
-	
+
+
+
 	////////////////////////////////////////////////////////////////////////////////// -> INSERT / UPDATE
-	
+
 	//Normale Datenbankanfrage
 	function query($query) {
 	  $this->querystring=$query;
-	  
+
 		$this->bench_start();
 	  $result = parent::query($query);
 		$this->bench_end();
-		
+
 		if ( !$result ) error($this->error());
 		else return $result;
-		
+
 	  return false;
 	}
-	
-	
-	
+
+
+
 	//DnyQuery Cols
 	function mkcols($data) {
 		$p = explode(',',trim($data));
@@ -96,37 +99,37 @@ class database extends mysqli {
 		}
 		return $out;
 	}
-	
-	
-	
+
+
+
 	//Dynamisches UPDATE
 	function dupdate($table, $postcols, $conditions="") {
 		$getcols = $this->mkcols($postcols);
 		$info = $this->fetch("SHOW COLUMNS FROM ".$table);
 		if ( !count($info) ) error('Anforderung der Tabellen-Spalten fehlgeschlagen<br />Tabelle: '.$table);
-		
+
 		$colcache = array();
 		foreach ( $info AS $thecol ) {
 			if ( in_array($thecol['Field'], $getcols) /*&& isset($_POST[$thecol['Field']])*/ ) {
 				$colcache[] = $table.".".$thecol['Field']."='".addslashes($_POST[$thecol['Field']])."'";
 			}
 		}
-		
+
 		$commands = implode(", ",$colcache);
-		
+
 		if ( $this->query("UPDATE ".$table." SET ".$commands." ".$conditions) ) return true;
 		return false;
 	}
-	
-	
-	
+
+
+
 	//Dynamischer INSERT
 	function dinsert($table,$postcols) {
 		$getcols=$this->mkcols($postcols);
-		
+
 		$info = $this->fetch("SHOW COLUMNS FROM ".$table);
 		if ( !count($info) ) error('Anforderung der Tabellen-Spalten fehlgeschlagen<br />Tabelle: '.$table);
-		
+
 		$colcache = array();
 		$valcache = array();
 		foreach ( $info AS $thecol ) {
@@ -135,26 +138,26 @@ class database extends mysqli {
 				$valcache[] = "'".addslashes($_POST[$thecol['Field']])."'";
 			}
 		}
-		
+
 		$cols = implode(", ", $colcache);
 		$values = implode(", ", $valcache);
-		
+
 		if ( $this->query("INSERT INTO ".$table." (".$cols.") VALUES (".$values.")") ) return true;
 		return false;
 	}
-	
-	
-	
-	//Insert durchführen
+
+
+
+	//Insert durchfï¿½hren
 	function insert($table, $values) {
 		$values = array_map('addslashes', $values);
 		$queryString = "INSERT INTO ".$table." (`".implode('`, `', array_keys($values))."`) VALUES ('".implode("','", $values)."')";
 		$this->query($queryString);
 	}
-	
-	
-	
-	//Insert durchführen
+
+
+
+	//Insert durchfï¿½hren
 	function update($table, $values, $where) {
 		$values = array_map('addslashes', $values);
 		$valueString = '';
@@ -165,143 +168,143 @@ class database extends mysqli {
 		$queryString = "UPDATE ".$table." SET ".$valueString." ".$where;
 		$this->query($queryString);
 	}
-	
-	
-	
-	
+
+
+
+
 	////////////////////////////////////////////////////////////////////////////////// -> SELECT
-	
+
 	//Select Datenbankanfrage, mehrzeilig
 	function fetch($query, $restype=0) {
 		$this->querystring = $query;
-		
+
 		if ( $restype==1 ) $restype = MYSQLI_ASSOC;
 		else $restype = MYSQLI_BOTH;
-	
+
 		$result = $this->query($query); //Query
 		if ( !$result ) return false;
-		
+
 		$fetched = array();
 		while ( $element = $result->fetch_array($restype) ) {
 			$fetched[] = $element;
 		}
 		$result->free();
-		
+
 		return $fetched;
 	}
-	
-	
-	
+
+
+
 	//Select Datenbankanfrage, mehrzeilig
 	function fetch_index($query, $indexcol, $restype=0) {
 		$this->querystring = $query;
-		
+
 		if ( $restype==1 ) $restype=MYSQLI_ASSOC;
 		else $restype=MYSQLI_BOTH;
-	
+
 		$result = $this->query($query); //Query
 		if ( !$result ) return false;
-		
+
 		$fetched = array();
 		while ( $element = $result->fetch_array($restype) ) {
 			if ( isset($element[$indexcol]) ) $fetched[$element[$indexcol]] = $element;
 			else $fetched[] = $element;
 		}
 		$result->free();
-		
+
 		return $fetched;
 	}
-	
-	
-	
+
+
+
 	//Select Datenbankanfrage, erste Zeile
 	function first($query, $restype=0) {
 		$this->querystring = $query;
-		
+
 		if ( $restype==1 ) $restype=MYSQLI_ASSOC;
 		else $restype=MYSQLI_BOTH;
-	
+
 		$result = $this->query($query); //Query
 		if ( !$result ) return false;
-		
+
 		$row = $result->fetch_array($restype);
 		$result->free();
-		
+
 		return $row;
 	}
-	
-	
-	
+
+
+
 	//Insert-Id
 	function insert_id() {
 		return $this->insert_id;
 	}
-	
-	
-	
+
+
+
 	//Affected Rows
 	function affected_rows() {
 		return $this->affected_rows;
 	}
-	
-	
-	
+
+
+
 	//Free Result
 	function free_result($query) {
 		$query->free();
 	}
-	
-	
-	
+
+
+
 	//Server-Info
 	function server_info() {
 		$this->server_info;
 	}
-	
-	
-	
+
+
+
 	////////////////////////////////////////////////////////////////////////////////// -> BENCHMARK
-	
+
 	function bench_start() {
 		if ( !$this->benchmark ) return;
 		$this->benchstart = microtime();
 	}
-	
-	
-	
+
+
+
 	function bench_end() {
 		if ( !$this->benchmark ) return;
-		
-		list($usec,$sec) = explode(' ',microtime()); 
+
+		list($usec,$sec) = explode(' ',microtime());
 		$b2 = ((float)$usec+(float)$sec);
-		list($usec,$sec) = explode(' ',$this->benchstart); 
+		list($usec,$sec) = explode(' ',$this->benchstart);
 		$b1 = ((float)$usec+(float)$sec);
 		$result = $b2-$b1;
-	  
+
 		$this->benchdata[] = array(
 			'query' => $this->querystring,
 			'time' => sprintf('%1.6f',$result)
 		);
-	  
+
 	  $this->benchtime += $b2-$b1;
 	}
-		
-		
+
+
 	////////////////////////////////////////////////////////////////////////////////// -> FEHLERMELDUNGEN
-	
+
 	//MySQL-Fehler -> Text
 	function error() {
 		if ( $this->quieterror ) return;
 		return 'Anfrage: '.$this->querystring.'<br />MySQL meldet: '.$this->error;
 	}
-	
-	
+
+
 	//Letzter Query
 	function lq() {
 		echo $this->querystring;
 	}
-	
-	
+
+
 } //END CLASS
 
 ?>
